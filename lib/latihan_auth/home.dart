13c68b90c5c46537d735c51ebf00fbbeba5c828e
irfnrdh <irfnrdh@purnamas-MacBook.local>
2020-02-10 14:47:31 +0700

    upp

--- lib/latihan_auth/home.dart
+++ lib/latihan_auth/home.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import '../hive_db.dart';
 
 class Home extends StatelessWidget {
-  // const Home({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     final _repo = Hivedb();
@@ -12,11 +10,15 @@ class Home extends StatelessWidget {
       appBar: AppBar(title: Text('Home')),
       body: FutureBuilder(
           future: _repo.itemHiveDb(),
-          builder: (context, snapshot) {
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (snapshot.hasData) {
               print(snapshot.data);
               return Center(
                 child: Text('Hello'),
+              );
+            } else if (snapshot.hasError) {
+              return Container(
+                child: Text('Error: ${snapshot.error}'),
               );
             } else {
               return Center(child: CircularProgressIndicator());
